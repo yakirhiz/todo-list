b3ac92c8503e3ae0287d345705bf0e43907a969f
Yakir H. <yakir@example.com>
2025-04-13 18:28:31 +0300

    Add payload to req object; added password method to auth

--- server/src/auth.js
+++ server/src/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const authenticate = async (req, res, next) => {
-    // Need to check that user exists in the database
     const authHeader = req.headers['authorization'];
 
     if (!authHeader || !authHeader.startsWith('Bearer'))
@@ -10,8 +10,11 @@ const authenticate = async (req, res, next) => {
     const token = authHeader.split(' ')[1];
 
     try {
-        const decode = jwt.verify(token, process.env.JWT_SECRET); // decode func vs verify func
-        // Need to verify with username (from req)
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        // Need to verify with username from token late on the routes
+        // Need to check that user exists in the database
+        // Otherwise deleted user w/ token can access authorized routes
+        req.user = payload;
         next();
     } catch (e) {
         res.status(401).json({ error: `Invalid token.` });
@@ -22,4 +25,14 @@ const generateToken = (username) => {
     return jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1hr' });
 };
 
-module.exports = { authenticate, generateToken };
+const verifyPassword = async (password, hash) => {
+    return await bcrypt.compare(password, hash);
+};
+
+const hashPassword = async (password) => {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    return hashedPassword;
+};
+
+module.exports = { authenticate, generateToken, verifyPassword, hashPassword };
