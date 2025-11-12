7bbfe0591ec5e51af6560c472755d9ef905eae3d
Yakir H. <yakir@example.com>
2025-11-12 16:34:30 +0200

    Add icons to operations in the todolist

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Modal from "./Modal";
 
+import { Plus } from 'lucide-react';
+
 export default function ListHeader({ listName, getData }) {
   const [showModal, setShowModal] = useState(false);
 
@@ -8,7 +10,9 @@ export default function ListHeader({ listName, getData }) {
     <div className="list-header">
       {/* <h1>{listName}</h1> */}
       <div className="button-container-top">
-        <button className="create" onClick={() => setShowModal(true)}>ADD NEW</button>
+        <button className="create" onClick={() => setShowModal(true)} title="Add New">
+          <Plus size={16} />
+        </button>
       </div>
       {showModal && <Modal mode={'create'} setShowModal={setShowModal} getData={getData} />}
     </div>
--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -4,6 +4,8 @@ import Modal from "./Modal";
 import ProgressBar from './ProgressBar';
 import { deleteTodo } from '../services/todosApi';
 
+import { Trash2, Pencil } from 'lucide-react';
+
 export default function ListItem({todo, getData}) {
   const [showModal, setShowModal] = useState(false);
 
@@ -26,8 +28,12 @@ export default function ListItem({todo, getData}) {
         <ProgressBar progress={todo.progress} />
       </div>
       <div className="button-container">
-        <button className="edit" onClick={() => setShowModal(true)}>EDIT</button>
-        <button className="delete" onClick={() => deleteData()}>DELETE</button>
+        <button className="edit" onClick={() => setShowModal(true)} title="Edit">
+          <Pencil size={16} />
+        </button>
+        <button className="delete" onClick={() => deleteData()} title="Delete">
+          <Trash2 size={16} />
+        </button>
       </div>
       {showModal && <Modal mode={'edit'} setShowModal={setShowModal} getData={getData} todo={todo} />}
     </li>
